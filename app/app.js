ed202e32778210901e49655cf3f7d12c9279d9de
hinote <jehinote@gmail.com>
2020-01-07 13:35:33 -0600

    Pushing changes for mobile and other tweaks

--- app/app.js
+++ app/app.js
@@ -10,6 +10,19 @@ const local_app = function () {}
 local_app.prototype.init = function (app) {
 	// express app available here
 	// don't forget these routes will be available on production server server (defaults to localhost:5000)
+
+	app.get('/testtemplate', function (req, res) {
+		enduro.api.temper.render('testtemplate', { whatsit: 'does this work?' })
+			.then((output) => {
+				res.send(output)
+			})
+	})
+
+	app.get('/hello', function (req, res) {
+		res.send('hello world')
+	})
 }
+
+
 
 module.exports = new local_app()
